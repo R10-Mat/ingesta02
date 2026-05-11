fd28d7811841317dfb09990828aa222e3e8cc9ce
R10-Mat <rayhan.matos@utec.edu.pe>
2026-05-10 19:04:19 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -12,12 +12,6 @@ MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_DEFAULT_REGION")
-
-S3_BUCKET = os.getenv("S3_BUCKET")
-
 conexion = pymysql.connect(
     host=MYSQL_HOST,
     user=MYSQL_USER,
@@ -35,14 +29,10 @@ df.to_csv(archivo_csv, index=False)
 
 print("CSV generado")
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_KEY,
-    region_name=AWS_REGION
-)
+s3 = boto3.client("s3")
+bucket = os.getenv("S3_BUCKET")
 
-s3.upload_file(archivo_csv, S3_BUCKET, archivo_csv)
+s3.upload_file(archivo_csv, bucket, archivo_csv)
 
 print("Archivo subido a S3")
 
